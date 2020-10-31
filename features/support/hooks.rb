28f3aa6bdc5f2a9fb949bf3cbbf83a98186e9d17
felipe89 <gfelipe.trindade@gmail.com>
2020-10-31 18:34:10 -0300

    Usando hooks na automacao

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,2 +1,13 @@
-require 'selenium-webdriver'
- 
+Before do
+   puts 'estou sendo executado antes de cada cenário.'
+   @soma = 5 + 5 
+end
+
+
+After do 
+    puts 'estou sendo executado depois de cada cenário.'
+end
+
+Before '@teste' do
+    puts 'rodei apenas para cenários com tags' 
+end
